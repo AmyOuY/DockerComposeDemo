dbcfd765ca8e77ecbebf8396c693f0165e858ae8
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2021-02-03 12:15:54 -0500

    Initial Commit

---
+++ WebAPI/Controllers/PeopleController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController
+    {
+        private readonly PeopleContext _db;
+
+        public PeopleController(PeopleContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var people = await _db.People.ToListAsync();
+
+            return new JsonResult(people);
+        }
+               
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var person = await _db.People.FirstOrDefaultAsync(p => p.Id == id);
+
+            return new JsonResult(person);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Person person)
+        {
+            _db.People.Add(person);
+            await _db.SaveChangesAsync();
+
+            return new JsonResult(person.Id);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, Person person)
+        {
+            var existingPerson = await _db.People.FirstOrDefaultAsync(p => p.Id == id);
+            existingPerson.FirstName = person.FirstName;
+            existingPerson.LastName = person.LastName;
+            existingPerson.Email = person.Email;
+            existingPerson.Phone = person.Phone;
+            var success = (await _db.SaveChangesAsync()) > 0;
+
+            return new JsonResult(success);
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var person = await _db.People.FirstOrDefaultAsync(p => p.Id == id);
+            _db.Remove(person);
+            var success = (await _db.SaveChangesAsync()) > 0;
+
+            return new JsonResult(success);
+        }
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebAPI.Models;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;          
+
+            try
+            {
+                var dbContext = services.GetRequiredService<PeopleContext>();
+                if (dbContext.Database.IsSqlServer())
+                {
+                    Console.WriteLine("Applying migration...");
+                    dbContext.Database.Migrate();
+                }
+            }
+            catch(Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while migrating or seeding the database!");
+
+                throw;
+            }
+
+            await host.RunAsync();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
